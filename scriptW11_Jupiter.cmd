065f5098e8f0819bb63e73298487174752104c00
Luca Alessandri <alessandri.luca91@gmail.com>
2025-05-12 09:58:17 +0200

    Delete scriptW11_Jupiter.cmd

--- scriptW11_Jupiter.cmd
+++
@@ -1,9 +0,0 @@
-//docker build --platform linux/amd64 . -t  informatica22-23
-//docker pull repbioinfo/informatica22-23
-@Set "Build=%CD%"
-@Echo(%Build%
-@If Not Exist "configurationFile.txt" Set /P "=%Build%" 0<NUL 1>"configurationFile.txt"
-mkdir %Build%
-copy configurationFile.txt %Build%
-del %Build%\id.txt
-docker run  --rm --platform linux/amd64 -itv %Build%:/sharedFolder --privileged=true -p  8888:8888 repbioinfo/informatica22-23
